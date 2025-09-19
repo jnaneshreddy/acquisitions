a4ae518cd73de28baf70637489999ba14cc62b0e
jnannesh reddy <jnaneshreddyv730@gmail.com>
2025-09-19 11:27:45 +0530

    added pretter and eslint scripts to make code clean and neat

--- src/app.js
+++ src/app.js
@@ -3,10 +3,8 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 app.get('/', (req, res) => {
-    res.status(200).send("hello from acquisitions");
-})
+  res.status(200).send('hello from acquisitions');
+});
 
 export default app;
-
